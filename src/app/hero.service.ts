4295630a01c0c60a8f088a952b210d40c35fa45e
Paynecheng <PayneChen@yeah.net>
2018-06-11 00:15:47 +0800

    add httpModule

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,35 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxj/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
+
+  private log(message: string) {
+    this.messageService.add('HeroService' +  message);
+  }
 
   getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
+    this.messageService.add('Heroservice: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`Heroservice: fetched hero id ${id}`);
+    return of(HEROES.find(item => item.id === id));
+  }
+
 }
